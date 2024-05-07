8cc14d16101e190f1140606f48b106e4efac48a3
hanyn1 <hanine.azizi@esprit.tn>
2024-05-08 00:06:38 +0200

    first step components done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TeacherSpace from "./components/TeacherSpace";
+import Profile from "./components/Profile";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import SeeCourse from "./components/SeeCourse";
+import SignUp from "./components/SignUp";
+import AddCourse from "./components/AddCourse";
+import DeleteCourse from "./components/DeleteCourse";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/signUp' element={<SignUp/>}/>
+          <Route path='/teacher' element={<TeacherSpace/>}/>
+          <Route path='/seeCourse' element={<SeeCourse/>}/>
+          <Route path='/Add' element={<AddCourse/>}/>
+          <Route path='/Delete' element={<DeleteCourse/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
